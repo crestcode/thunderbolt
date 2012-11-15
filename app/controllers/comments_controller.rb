0bbadbefbbdf8dafb31d6aaeb17d9f94546231ec
J. Michael Kasiewicz <crestcode@gmail.com>
2012-11-14 20:17:16 -0500

    Modified comment controller to correctly save valid comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.build(params[:comment])
 		if @comment.save
-			redirect_to post_path(post)
+			redirect_to post_path(@post)
 		else
 			@post.reload
       render 'posts/show'
