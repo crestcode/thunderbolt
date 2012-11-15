bf195217f61df63d4a45abfad7f3127b3686b233
J. Michael Kasiewicz <crestcode@gmail.com>
2012-11-14 20:12:36 -0500

    Fixed comment validation issue when user submits comment smaller than two characters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,12 @@
 class CommentsController < ApplicationController
 	
 	def create
-		post = Post.find(params[:post_id])
-		comment = post.comments.create(params[:comment])
-		if comment.valid?
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.build(params[:comment])
+		if @comment.save
 			redirect_to post_path(post)
 		else
-			@post = Post.find(params[:post_id])
-			@comment = @post.comments.create(params[:comment])
-			@comment.content = nil
+			@post.reload
       render 'posts/show'
     end
 	end
